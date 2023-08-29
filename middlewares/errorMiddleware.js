392fcdb0c608da3ef77c4d75545772dad5daca40
Abylaikhan Julmagambetov <khan@Abylaikhans-MacBook-Pro.local>
2023-08-28 19:58:23 -0700

    showing stack trace now and removed build of client on heroku

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -10,7 +10,8 @@ const errorHandler = (err, req, res, next) => {
 
   res.status(statusCode).json({
     message,
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    // stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    stack: err.stack,
   })
 }
 
