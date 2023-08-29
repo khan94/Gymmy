6f4487fbff3d93b61d421596bd78ea7afee5ddaf
Abylaikhan Julmagambetov <khan@Abylaikhans-MacBook-Pro.local>
2023-08-28 19:50:54 -0700

    removed all the stuff from cors settings

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
-import express from "express";
+import express from 'express'
 // import compression from "compression";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 // routes
-import workoutRouter from "./routes/workoutRoute.js";
+import workoutRouter from './routes/workoutRoute.js'
 // const workoutRouter = require('./routes/workoutRoute')
-import exerciseRouter from "./routes/exerciseRoute.js";
+import exerciseRouter from './routes/exerciseRoute.js'
 // const exerciseRouter = require('./routes/exerciseRoute')
-import setRouter from "./routes/setRoute.js";
-import userRouter from "./routes/userRoute.js";
-import dotenv from "dotenv";
-import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
+import setRouter from './routes/setRoute.js'
+import userRouter from './routes/userRoute.js'
+import dotenv from 'dotenv'
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js'
 
-dotenv.config();
-const app = express();
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+dotenv.config()
+const app = express()
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // Set default port for express app
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4001
 
-app.use(
-  cors({
-    credentials: true,
-    // origin: "http://localhost:3000",
-    // origin: "https://shimmering-cranachan-8a7432.netlify.app",
-    origin: "https://gymmy-umber.vercel.app/",
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     // origin: "http://localhost:3000",
+//     // origin: "https://shimmering-cranachan-8a7432.netlify.app",
+//     origin: "https://gymmy-umber.vercel.app/",
+//   })
+// );
+
+app.use(cors())
 // app.use(compression());
 
-app.use("/api/users", userRouter);
-app.use("/api/workouts", workoutRouter);
-app.use("/api/exercises", exerciseRouter);
-app.use("/api/sets", setRouter);
+app.use('/api/users', userRouter)
+app.use('/api/workouts', workoutRouter)
+app.use('/api/exercises', exerciseRouter)
+app.use('/api/sets', setRouter)
 
-app.use(notFound);
-app.use(errorHandler);
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
-  console.log("server is running on port: ", PORT);
-});
+  console.log('server is running on port: ', PORT)
+})
